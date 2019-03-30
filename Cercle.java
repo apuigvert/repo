fd2cd061708e03e06d5ffdc5e9bc37d3146b899e
apuigvert <puigvert.s.adria@gmail.com>
2019-03-30 15:31:47 +0100

    integrate master

--- Cercle.java
+++ Cercle.java
@@ -1,10 +1,22 @@
 public class Cercle{
  private float x;
  private float y;
- Cercle(float xx, float yy)
- {
+ Cercle(float xx, float yy){
  x=xx;
  y=yy;
+ }
+ public float getX() {
+    return x;
+}
+
+public void setX(float x) {
+    this.x = x;
+}
+public float getY(){
+    return y;
+}
+public void setY (float y){
+    this.y = y;
+}
 
  }
-}
